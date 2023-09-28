9ab8b7a4e735295d1c6f4dfa2993fea969891007
devProMaleek <abdulmalikadebayo07@gmail.com>
2023-09-28 11:46:33 +0100

    learn: Create a program for a ticketing station

--- day-3-conditional-statement/lesson.py
+++ day-3-conditional-statement/lesson.py
@@ -1,16 +1,26 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
-
+total_bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster")
     # Nested if-else
     age = int(input("What is your age? "))
     if age <= 12:
-        print("Please pay $5, and enjoy your ride.")
+        total_bill += 5
+        print("Child tickets are $5.")
     elif age > 12 and age < 19:
-        print("Please pay $7, and enjoy your ride.")
+        total_bill += 7
+        print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!")
     else:
-        print("Please pay $12, and enjoy your ride.")
+        total_bill += 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ").lower()
+    if wants_photo == "y":
+        total_bill += 3
+        print(f"Your total bill is ${total_bill}. Thank you and enjoy the ride!")
 else:
-    print("Sorry, you have to grow taler before your ride")
+    print("Sorry, you have to grow taller before you can ride")
