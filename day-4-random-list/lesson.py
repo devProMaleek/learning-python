acb9b2e9190e6456b4014ec1b2106f3053a78fc1
devProMaleek <abdulmalikadebayo07@gmail.com>
2023-10-04 11:20:56 +0100

    learn: Lesson day 4

---
+++ day-4-random-list/lesson.py
@@ -0,0 +1,11 @@
+import random
+
+# Random integer between 1 and 10
+random_integer = random.randint(1, 5)
+# print(random_integer)
+
+# Random float between 0 and 1
+random_float = random.random()
+
+random_float_between_5 = random_float * 5
+print(random_float_between_5)
